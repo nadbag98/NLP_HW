7b879bfcb52cdfa06a48f3d4fcb225f03e0b0cdd
nadbag98 <nadavgat@mail.tau.ac.il>
2021-12-26 13:06:26 +0200

    1 up to half of e

--- parser_transitions.py
+++ parser_transitions.py
@@ -15,32 +15,39 @@ class PartialParse(object):
         @param sentence (list of str): The sentence to be parsed as a list of words.
                                         Your code should not modify the sentence.
         """
-        # The sentence being parsed is kept for bookkeeping purposes. Do not alter it in your code.
+        # The sentence being parsed is kept for bookkeeping purposes.
+        # Do not alter it in your code.
         self.sentence = sentence
 
         ### TODO:
         ### Your code should initialize the following fields:
-        ###     self.stack: The current stack represented as a list with the top of the stack as the
+        ###     self.stack: The current stack represented as a list with the top
+        # of the stack as the
         ###                 last element of the list.
-        ###     self.buffer: The current buffer represented as a list with the first item on the
+        ###     self.buffer: The current buffer represented as a list with the
+        # first item on the
         ###                  buffer as the first item of the list
-        ###     self.dependencies: The list of dependencies produced so far. Represented as a list of
+        ###     self.dependencies: The list of dependencies produced so far.
+        # Represented as a list of
         ###             tuples where each tuple is of the form (head, dependent).
         ###             Order for this list doesn't matter.
         ###
         ### Note: The root token should be represented with the string "ROOT"
         ###
         ### YOUR CODE HERE (3 Lines)
-        raise NotImplementedError
+        self.stack = ["ROOT"]
+        self.buffer = self.sentence.copy()
+        self.dependencies = []
         ### END YOUR CODE
 
 
     def parse_step(self, transition):
-        """Performs a single parse step by applying the given transition to this partial parse
+        """Performs a single parse step by applying the given transition to
+        this partial parse
 
-        @param transition (str): A string that equals "S", "LA", or "RA" representing the shift,
-                                left-arc, and right-arc transitions. You can assume the provided
-                                transition is a legal transition.
+        @param transition (str): A string that equals "S", "LA", or "RA"
+        representing the shift, left-arc, and right-arc transitions.
+        You can assume the provided transition is a legal transition.
         """
         ### TODO:
         ###     Implement a single parsing step, i.e. the logic for the following as
@@ -49,7 +56,15 @@ class PartialParse(object):
         ###         2. Left Arc
         ###         3. Right Arc
         ### YOUR CODE HERE (~7-10 Lines)
-        raise NotImplementedError
+        if transition == "S":
+            self.stack.append(self.buffer[0])
+            self.buffer.pop(0)
+        elif transition == "LA":
+            dependent = self.stack.pop(-2)
+            self.dependencies.append((self.stack[-1], dependent))
+        elif transition == "RA":
+            dependent = self.stack.pop(-1)
+            self.dependencies.append((self.stack[-1], dependent))
         ### END YOUR CODE
 
     def parse(self, transitions):
@@ -70,13 +85,19 @@ def minibatch_parse(sentences, model, batch_size):
     """Parses a list of sentences in minibatches using a model.
 
     @param sentences (list of list of str): A list of sentences to be parsed
-                                            (each sentence is a list of words and each word is of type string)
-    @param model (ParserModel): The model that makes parsing decisions. It is assumed to have a function
-                                model.predict(partial_parses) that takes in a list of PartialParses as input and
-                                returns a list of transitions predicted for each parse. That is, after calling
+                                            (each sentence is a list of words
+                                            and each word is of type string)
+    @param model (ParserModel): The model that makes parsing decisions.
+                                It is assumed to have a function
+                                model.predict(partial_parses) that takes in a
+                                list of PartialParses as input and
+                                returns a list of transitions predicted for each
+                                parse. That is, after calling
                                     transitions = model.predict(partial_parses)
-                                transitions[i] will be the next transition to apply to partial_parses[i].
-    @param batch_size (int): The number of PartialParses to include in each minibatch
+                                transitions[i] will be the next transition to
+                                apply to partial_parses[i].
+    @param batch_size (int): The number of PartialParses to include in each
+    minibatch
 
 
     @return dependencies (list of dependency lists): A list where each element is the dependencies
@@ -89,17 +110,29 @@ def minibatch_parse(sentences, model, batch_size):
     ### TODO:
     ###     Implement the minibatch parse algorithm as described in the pdf handout
     ###
-    ###     Note: A shallow copy (as denoted in the PDF) can be made with the "=" sign in python, e.g.
+    ###     Note: A shallow copy (as denoted in the PDF)
+    # can be made with the "=" sign in python, e.g.
     ###                 unfinished_parses = partial_parses[:].
     ###             Here `unfinished_parses` is a shallow copy of `partial_parses`.
-    ###             In Python, a shallow copied list like `unfinished_parses` does not contain new instances
-    ###             of the object stored in `partial_parses`. Rather both lists refer to the same objects.
-    ###             In our case, `partial_parses` contains a list of partial parses. `unfinished_parses`
-    ###             contains references to the same objects. Thus, you should NOT use the `del` operator
-    ###             to remove objects from the `unfinished_parses` list. This will free the underlying memory that
-    ###             is being accessed by `partial_parses` and may cause your code to crash.
+    ###             In Python, a shallow copied list like `unfinished_parses`
+    # does not contain new instances
+    ###             of the object stored in `partial_parses`. Rather both lists
+    # refer to the same objects.
+    ###             In our case, `partial_parses` contains a list of partial
+    # parses. `unfinished_parses` contains references to the same objects.
+    # Thus, you should NOT use the `del` operator
+    ###             to remove objects from the `unfinished_parses` list.
+    # This will free the underlying memory that
+    ###             is being accessed by `partial_parses` and may cause your
+    # code to crash.
     ### YOUR CODE HERE (~8-10 Lines)
-    raise NotImplementedError
+    partial_parses = [PartialParse(sentences[i]) for i in range(len(sentences))]
+    unfinished_parses = partial_parses[:]
+    while unfinished_parses != []:
+        minibatch = unfinished_parses[:batch_size]
+        transitions = model.predict(minibatch)
+        for i in range(batch_size):
+            minibatch[i].parse_step(transitions[i])
     ### END YOUR CODE
 
     return dependencies
